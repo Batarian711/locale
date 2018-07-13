21b3c72a0b263eaaceef84f7e7495ca88e4cdef1
Matt Clarkson <matthew.clarkson@ef.com>
2018-07-13 17:07:22 +0100

    test(*): test all locales

--- test/Locale.test.ts
+++ test/Locale.test.ts
@@ -1,17 +1,51 @@
-import { enGB, enUS } from '@lib';
+import { ILocale, Iso3166, Iso639, locale as locales, Script } from '@lib';
+
+class Locale implements ILocale {
+  private readonly locale!: ILocale;
+  language!: Iso639;
+  region!: Iso3166;
+  script!: Script;
+  constructor(locale: ILocale) {
+    Object.defineProperties(this, {
+      locale: { value: locale },
+      language: { get: () => this.locale.language, enumerable: true },
+      region: { get: () => this.locale.region, enumerable: true },
+      script: { get: () => this.locale.script, enumerable: true },
+    });
+  }
+
+  toString(): string {
+    const { language, region, script } = this.locale;
+    if (region && script) {
+      return `${language}${region}${script}`;
+    } else if (region) {
+      return `${language}${region}`;
+    } else if (script) {
+      return `${language}${script}`;
+    } else {
+      return language;
+    }
+  }
+}
 
 describe('Locale', () => {
-  describe('enGB', () => {
-    it('should be be a valid language code', () => {
-      expect(enGB).toHaveProperty('language', 'en');
-      expect(enGB).toHaveProperty('region', 'GB');
+  describe.each(Array.from(locales, l => new Locale(l)))('%s', locale => {
+    it('should have a language property', () => {
+      expect(locale).toHaveProperty('language');
+      const { language } = locale;
+      expect(typeof language === 'string').toBe(true);
     });
-  });
 
-  describe('enUS', () => {
-    it('should be be a valid language code', () => {
-      expect(enUS).toHaveProperty('language', 'en');
-      expect(enUS).toHaveProperty('region', 'US');
+    it('should have an optional region property', () => {
+      expect(locale).toHaveProperty('region');
+      const { region } = locale;
+      expect(region === undefined || typeof region === 'string').toBe(true);
+    });
+
+    it('should have an optional script property', () => {
+      expect(locale).toHaveProperty('script');
+      const { script } = locale;
+      expect(script === undefined || typeof script === 'string').toBe(true);
     });
   });
 });
